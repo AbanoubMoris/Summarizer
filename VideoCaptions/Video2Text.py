f6a320c39a28406757d6328964fa1a901154a36e
abanoub moris <abanoub.moris1@gmail.com>
2021-07-02 20:29:42 +0200

    Video2Text

---
+++ VideoCaptions/Video2Text.py
@@ -0,0 +1,51 @@
+import speech_recognition as sr 
+import moviepy.editor as mp
+
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from moviepy.editor import VideoFileClip
+
+class VideoCaptions:
+
+  input_video_path = 'video.mp4'
+  output_video_path = 'converted.mp4'
+  result = ''
+
+  def getVideosDuration(self,input_video_path):
+    video = VideoFileClip(input_video_path)
+    video_duration = int(video.duration)
+    print(video_duration)
+    return video_duration
+
+  def getChunkCaption(self):
+    i = 0
+    while i < self.getVideosDuration(self.input_video_path):
+        
+        try:
+          with VideoFileClip(self.input_video_path) as video:
+            #First 10 seconds
+            new = video.subclip(i, i+9)
+            new.write_videofile(self.output_video_path, audio_codec='aac')
+            i+=10
+        except:
+          break
+
+        clip = mp.VideoFileClip(r"converted.mp4".format(i+1)) 
+        clip.audio.write_audiofile(r"converted.wav".format(i+1))
+        r = sr.Recognizer()
+        audio = sr.AudioFile("converted.wav".format(i+1))
+        with audio as source:
+          r.adjust_for_ambient_noise(source)  
+          audio_file = r.record(source)
+        self.result += r.recognize_google(audio_file)+' '
+
+        
+  def WriteCaptionInFile(self):
+    with open('recognized.txt',mode ='w') as file: 
+          file.write(self.result) 
+          file.write("\n") 
+          print("Finally ready!")
+    return self.result
+
+captions = VideoCaptions()
+captions.getChunkCaption()
+print(captions.WriteCaptionInFile())
